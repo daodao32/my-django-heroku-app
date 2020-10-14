aab90dd3691ac36b2161ed89550f55e993bed936
daodao32 <ubuntu@ubuntu>
2020-10-13 23:08:34 -0400

    a

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ docker build -t django-duan -f Dockerfile .
 # push your directory container for the web process to heroku
 heroku container:push web -a django-duan
 
-
 # promote the web process with your container 
 heroku container:release web -a django-duan
 
